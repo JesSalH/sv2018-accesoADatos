3c0069f0e4fbd98d8179d1c02bd420a2f1d1ff52
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-10 12:27:59 +0200

    Ejercicio 01.14: Invertir una imagen NetPbm

---
+++ tema01-ficheros/Exercise0114-InvertNetPbm.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+// Invert a PBM image
+// Javier Saorín Vidal.
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Exercise0114 {
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String fileName = sc.nextLine();        
+       
+        try {
+            InputStream input = new FileInputStream(new File(fileName));
+            OutputStream output = new FileOutputStream(fileName + ".rev.pgm");
+            
+            int length = input.available();
+            
+            byte[] buf = new byte[length];
+            
+            int cantidadLeida = 0;
+            if ((cantidadLeida = input.read(buf, 0, length)) == length) {
+                
+                int jumps = 0;
+                int headerCount = 0;
+                
+                while(jumps < 3) {
+                    if(buf[headerCount] == 10)
+                        jumps++;
+                    headerCount++;
+                }
+                
+                for (int b = 0; b < headerCount; b++) {
+                    output.write(buf[b]);
+                }
+                
+                for (int i = buf.length-1; i >= headerCount; i--) {
+                    output.write(buf[i]);
+                }
+            }
+            
+            input.close();
+            output.close();
+        } catch (IOException e) {
+            System.err.println("Input/Output ERROR: " + e.getMessage());
+        }
+    }
+}
