7d3d4a3d2afd1561f4f523726d236b46d638f71f
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-24 23:29:01 +0200

    Ejercicio 01.24b: Segundo ejemplo de XML con DOM

---
+++ tema01-ficheros/Exercise0124b-Dom2.java
@@ -0,0 +1,40 @@
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+//Jairo Pérez Sánchez
+
+public class ReadXmlDOM {
+    public static void main(String[] args){
+        try {
+            File inputFile = new File("asignaturas.xml");
+            DocumentBuilderFactory dbFactory 
+                = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+                        NodeList nList = doc.getElementsByTagName("asignatura");
+            
+            System.out.println("Buscando asignaturas de segundo..."); 
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    if(eElement.getElementsByTagName("curso")
+                            .item(0).getTextContent().equals("Segundo")) {
+                        System.out.println("Nombre: " 
+                            + eElement.getElementsByTagName("nombre")
+                            .item(0).getTextContent());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
