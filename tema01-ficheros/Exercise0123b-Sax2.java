952ac613badf244c85055e03a80a758c5843b6b9
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-24 23:08:08 +0200

    Ejercicio 01.23b: Segundo ejemplo de SAX

---
+++ tema01-ficheros/Exercise0123b-Sax2.java
@@ -0,0 +1,67 @@
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+//Almudena Lopez Sanchez
+
+class ReadXml extends DefaultHandler {
+
+    public void procesarXml() {
+        try {
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            DefaultHandler manejadorEventos = new DefaultHandler() {
+
+                String etiquetaActual = "";
+                String contenido = "";
+                String asignaturaActual = "";
+
+                // Método que se llama al encontrar inicio de etiqueta: '<'
+                public void startElement(String uri, String localName, String qName, Attributes attributes)
+                        throws SAXException {
+                    etiquetaActual = qName; 
+                }
+
+                // Obtiene los datos entre '<' y '>'
+                public void characters(char ch[], int start, int length) throws SAXException {
+
+                    contenido = new String(ch, start, length);
+                }
+
+                // Llamado al encontrar un fin de etiqueta: '>'
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+
+                    if (etiquetaActual != "") {
+                        if(etiquetaActual.equals("nombre"))
+                        {
+                            asignaturaActual = contenido;
+                        }
+                        else if(etiquetaActual.equals("curso"))
+                        {
+                            if(contenido.equalsIgnoreCase("segundo"))
+                            {
+                                System.out.println("Nombre asignatura" + ": " + asignaturaActual);
+                            }
+                        }
+                        etiquetaActual = "";
+                    }
+                }
+            };
+
+            // Cuerpo de la función: trata de analizar el fichero deseado
+            // Llamará a startElement(), endElement() y character()
+            saxParser.parse("asignaturas.xml", manejadorEventos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Exercise0123{
+    public static void main(String args[]) {
+        ReadXml ficheroXml = new ReadXml();
+        ficheroXml.procesarXml();
+    }
+}
