633cd80b831c282c5a524a1e0373b3bfb76092e1
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-11-21 16:55:31 +0100

    Contacto con PHP + MySQL: mostrar datos

---
+++ tema02-accesoABasesDeDatos/php/05-mostrar/mostrar.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Mostrar datos</title>
+        <meta charset="utf-8" />
+    </head>
+    
+    <body>
+        <h1>Mostrar datos</h1>
+        <p>
+            <?php
+            $host ="localhost";
+            $usuario = "root";
+            $password = "";
+            $baseDeDatos = "adatos";
+            $mysqli = new mysqli($host,
+                $usuario, $password, $baseDeDatos);
+            
+            if ($mysqli->connect_errno) 
+            {
+                echo "Ha fallado la conexión: ". $mysqli->connect_error;
+                echo "</p></body></html>";
+                exit();
+            }
+            
+            $mysqli->query("SET NAMES utf8;");
+
+            if ($resultado = $mysqli->query(
+                "SELECT * FROM alumnos"))
+            {
+                echo "Cantidad de filas encontradas: " . $resultado->num_rows;
+                    
+                while ($registro = $resultado->fetch_array())
+                {
+                    echo $registro["nombre"] . " " .
+                        $registro["anyoNacimiento"] . "<br />";
+                }
+            }
+            $resultado->close();
+           ?>
+        </p>
+    </body>
+</html>
