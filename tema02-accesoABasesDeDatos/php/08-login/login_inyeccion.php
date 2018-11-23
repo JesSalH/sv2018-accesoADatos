096901a2ce04e305a7c024e2ca758c4632c45b92
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-23 10:29:02 +0100

    Contacto con PHP + MySQL: 08, ejemplo de login (e inyecc. SQL)

---
+++ tema02-accesoABasesDeDatos/php/08-login/login_inyeccion.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8" />
+    </head>
+    
+    <body>
+<?php
+if (! isset($_POST["nombre"])) 
+{
+?>  
+    
+        <h1>Login</h1>
+        <form method="post"> 
+            <input type="text" name="nombre" placeholder="Nombre" />
+            <br />
+            <input type="text" name="anyo" placeholder="Password" />
+            <br />
+            <input type="submit" value="Buscar" />
+        </form>
+<?php
+}
+else
+{
+    $host ="localhost";
+    $usuario = "root";
+    $password = "root";
+    $baseDeDatos = "adatos";
+    $mysqli = new mysqli($host,
+        $usuario, $password, $baseDeDatos);
+    
+    if ($mysqli->connect_errno) 
+    {
+        echo "Ha fallado la conexión: ". $mysqli->connect_error;
+        echo "</p></body></html>";
+        exit();
+    }
+    
+    // Cuidado: tomar datos sin procesar puede dar lugar a
+    // ataques de inyección de SQL
+    $nombre = $_POST["nombre"]; 
+    $password = $_POST["anyo"];
+    
+    $mysqli->query("SET NAMES utf8;");
+
+    $busqueda = $_POST["nombre"];
+    $orden = "SELECT * FROM alumnos
+        WHERE nombre ='". $nombre ."'
+        AND anyoNacimiento ='". $password ."'";
+    
+    echo $orden;
+    if ($resultado = $mysqli->query($orden))
+    {
+        if ($resultado->num_rows > 0)
+            echo "Welcome! ;-)";
+        else
+            echo "Va a ser que no";
+        /*
+        while ($registro = $resultado->fetch_array())
+        {
+            echo $registro["nombre"] . " " .
+                $registro["anyoNacimiento"] . "<br />";
+        }
+        */
+    }
+    $resultado->close();
+
+}
+?>  
+
+    </body>
+</html>
